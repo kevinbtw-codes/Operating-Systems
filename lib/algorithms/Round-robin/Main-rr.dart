2528c36d11718c4fe5a1e538e10fd9eaa36294d7
khushi <khushi.sce19@sot.pdpu.ac.in>
2021-04-08 18:21:21 +0530

    process table

--- lib/algorithms/Round-robin/Main-rr.dart
+++ lib/algorithms/Round-robin/Main-rr.dart
@@ -3,7 +3,7 @@ import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:os_project/Algorithm%20page.dart';
 // import 'round-robin-algo.dart';
 import 'round-robin.dart';
-//import 'table.dart';
+import 'table.dart';
 import 'gantt.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'dart:async';
@@ -355,7 +355,7 @@ class _AlgorithmState extends State<Algorithm> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    //builder: (context) => TheTable(prs),
+                    builder: (context) => TheTable(prs),
                     ),
               );
             },
